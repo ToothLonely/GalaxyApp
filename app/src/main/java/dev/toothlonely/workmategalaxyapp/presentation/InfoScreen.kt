5573ee8c4f7c31fae902e5e22f0fd4eccd1a6f6a
Tooth Lonely <arsen.shirinov2004@gmail.com>
2025-10-17 11:26:27 +0300

    WGA-5 added info screen

--- app/src/main/java/dev/toothlonely/workmategalaxyapp/presentation/InfoScreen.kt
+++ app/src/main/java/dev/toothlonely/workmategalaxyapp/presentation/InfoScreen.kt
@@ -1,11 +1,42 @@
 package dev.toothlonely.workmategalaxyapp.presentation
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 
 @Composable
 fun InfoScreen() {
-    Column {
 
+    val url = "https://masterpiecer-images.s3.yandex.net/5fcb1cda5223d2d:upscaled"
+    val description =
+        "The explosion is over but the consequences continue. About eleven thousand years ago a star in the constellation of Vela could be seen to explode, creating a strange point of light briefly visible to humans living near the beginning of recorded history. The outer layers of the star crashed into the  interstellar medium, driving a shock wave that is still visible today. A roughly spherical, expanding shock wave is visible in X-rays. The above image captures some of that filamentary and gigantic shock in visible light. As gas flies away from the detonated star, it decays and reacts with the interstellar medium, producing light in many different colors and energy bands. Remaining at the center of the Vela Supernova Remnant is a pulsar, a star as dense as nuclear matter that rotat an ten times in a single second."
+    val title = "Filaments of the Vela Supernova Remnant"
+
+    Column(
+        modifier = Modifier.verticalScroll(rememberScrollState())
+    ) {
+        AsyncImage(
+            model = url,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(Modifier.height(50.dp))
+
+        Text(text = title)
+
+        Spacer(Modifier.height(35.dp))
+
+        Text(text = description)
     }
 }
--- app/src/main/java/dev/toothlonely/workmategalaxyapp/presentation/MainScreen.kt
+++ app/src/main/java/dev/toothlonely/workmategalaxyapp/presentation/MainScreen.kt
@@ -1,8 +1,6 @@
 package dev.toothlonely.workmategalaxyapp.presentation
 
-import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,11 +8,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
@@ -22,16 +18,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.paging.compose.collectAsLazyPagingItems
 import coil3.compose.AsyncImage
-import coil3.request.ImageRequest
 import dev.toothlonely.workmategalaxyapp.ui.theme.WorkmateGalaxyAppTheme
 
 @Composable
